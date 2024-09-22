db0f610515e9612d874714a04e94d416d9bc6a97
Dumbie <wasleyron668@gmail.com>
2024-09-22 18:26:27 +0500

    backend lesson 2: mongodb

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const postModel = require('./models/post.model')
 
 const app = express()
 
-const PORT = 8080
+app.get('/', async (req, res) => {
+	try {
+		const allPosts = await postModel.find()
+		res.status(200).json(allPosts)
+	} catch (error) {
+		res.status(500).json(error)
+	}
+	res.send('Hello Sammi')
+})
 
-app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`))
+app.post('/', async (req, res) => {
+	try {
+		const { title, body } = req.body
+		const newPost = await postModel.create({ title, body })
+		res.status(201).json(newPost)
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
+app.get('/post', (req, res) => {
+	res.json({ message: 'Hello Port' })
+})
+
+const PORT = process.env.PORT
+
+const bootstrap = async () => {
+	try {
+		await mongoose
+			.connect(process.env.DB_URL)
+			.then(() => console.log('Connected DB'))
+		app.listen(PORT, () =>
+			console.log(`Listening on - http://localhost:${PORT}`)
+		)
+	} catch (error) {
+		console.log(`Error connecting with DB: ${error}`)
+	}
+}
+
+bootstrap()
